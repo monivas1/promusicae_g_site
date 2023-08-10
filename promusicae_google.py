af32a46e8b55069e54ea3e19a6609295577b5b17
monivas1 <66659851+monivas1@users.noreply.github.com>
2023-08-10 16:20:44 +0200

    Update promusicae_google.py

--- promusicae_google.py
+++ promusicae_google.py
@@ -73,7 +73,6 @@ def get_beau_with_selbase(URL):
         
        
         soup = BeautifulSoup(source, "lxml")
-        st.write(soup)
         driver.quit()
         
  #   except AttributeError:
